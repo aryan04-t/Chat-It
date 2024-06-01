3810881c43af5db8dc45a6305cb3914d349bb1c5
Aryan Tomar <aryantomar5000@gmail.com>
2024-06-01 08:16:12 +0530

    Implemented a Responsive Home Chatting Layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react'
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { useDispatch, useSelector } from 'react-redux'
 import { setUser, logout } from '../redux/userSlice'
 import toast from 'react-hot-toast'
 
 import Sidebar from '../components/Sidebar'
+import favicon from '../assets/favicon.png'
 
 
 const Home = () => {
@@ -14,6 +15,7 @@ const Home = () => {
 	const user = useSelector(state => state.user); 
 	
 	const navigate = useNavigate(); 
+	const location = useLocation(); 
 
 	const fetchUserDetails = async () => {
 		axios({
@@ -34,18 +36,39 @@ const Home = () => {
 	} 
 
 	useEffect( () => {
-		fetchUserDetails();
+
+		if(user.name === ''){
+			navigate('/login-email'); 
+		}
+		else{
+			fetchUserDetails();
+		}
+
 	}, [])
+
+
+	const basePath = location.pathname === '/'; 
 
 	return (
 		<div className='grid lg:grid-cols-[400px,1fr] h-screen max-h-screen'>
-			<section className='bg-blue-500'>
+			<section className={`bg-zinc-800 ${!basePath && 'hidden'} lg:block`}>
 				<Sidebar />
 			</section>
 
-			<section>
+			<section className={`${basePath && 'hidden'}`}>
 				<Outlet />
 			</section>
+
+			<div className={`hidden lg:${!basePath && 'hidden'} lg:${basePath && 'block'} h-screen max-h-screen select-none`}>
+				<div className='select-none h-full w-full flex flex-col items-center justify-center'> 
+					<img
+						src={favicon}
+						height={250}
+						width={250}
+					/>
+					<p className='text-white text-3xl font-serif mt-2 text-center'> Select user to send texts </p>
+				</div>
+			</div>
 		</div>
 	)
 }
